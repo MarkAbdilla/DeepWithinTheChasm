49faf97824812b2c8ecd5413f09e52ba92a02ce1
DESKTOP-N3S839F\Mark <markabdilla26@gmail.com>
2020-05-08 16:28:51 +0200

    Raycasting and enemy health implemented

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] Camera fpCamera;
+    [SerializeField] float range = 100f;
+    [SerializeField] float weaponDamage = 10f;
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(fpCamera.transform.position, fpCamera.transform.forward, out hit, range);
+        if (hasHit)
+        {
+            EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
+            target.TakeDamage(weaponDamage);
+        }
+    }
+}
