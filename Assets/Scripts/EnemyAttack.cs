75182ff18f74c3927d5662444fbe2fcbc651caae
DESKTOP-N3S839F\Mark <markabdilla26@gmail.com>
2020-05-09 15:44:38 +0200

    Player Health implementation and animation fix

--- Assets/Scripts/EnemyAttack.cs
+++ Assets/Scripts/EnemyAttack.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 
 public class EnemyAttack : MonoBehaviour
 {
-    [SerializeField] Transform target;
     [SerializeField] float damage = 40f;
+
+    PlayerHealth target;
 
     void Start()
     {
-
+        target = FindObjectOfType<PlayerHealth>();
     }
 
     public void AttackHitEvent()
     {
         if (target == null) { return; }
         Debug.Log("HIT!");
+        target.GetComponent<PlayerHealth>().DamagePlayer(damage);
     }
 }
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] float playerHitPoints = 100f;
+
+    public void DamagePlayer(float damage)
+    {
+        playerHitPoints -= damage;
+        if (playerHitPoints <= 0)
+        {
+            print("You dead nigga");
+        }
+    }
+}
