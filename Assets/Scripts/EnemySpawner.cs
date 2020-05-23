0c9f916287cc390c53ea1eaf6c269fb6b80b12cf
DESKTOP-N3S839F\Mark <markabdilla26@gmail.com>
2020-05-23 17:10:20 +0200

    Enemy Spawner and chasm lights

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] EnemyAI enemyPrefab;
+    [SerializeField] Transform[] enemySpawnPositions;
+    [SerializeField] float spawnDelay = 5f;
+
+    int enemyPosition;
+
+    private void Start()
+    {
+        enemyPosition = UnityEngine.Random.Range(0, enemySpawnPositions.Length);
+    }
+
+    private void Update()
+    {
+        StartCoroutine(SpawnZombies());
+    }
+
+    IEnumerator SpawnZombies()
+    {
+        Instantiate(enemyPrefab, enemySpawnPositions[enemyPosition]);
+        enemyPosition = UnityEngine.Random.Range(0, enemySpawnPositions.Length);
+        yield return new WaitForSeconds(spawnDelay);
+    }
+}
