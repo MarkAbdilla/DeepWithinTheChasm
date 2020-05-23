6ad82e64af5449093f16b19cae2981d2b11284b7
DESKTOP-N3S839F\Mark <markabdilla26@gmail.com>
2020-05-23 10:44:33 +0200

    Timer implementation and main menu setup

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -6,10 +6,30 @@ using UnityEngine.SceneManagement;
 public class SceneLoader : MonoBehaviour
 {
 
-    public void ReloadScene()
+    public void StartGame()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void LoadInstructions()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadMainMenu()
     {
         SceneManager.LoadScene(0);
+    }
+
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(2);
         Time.timeScale = 1;
+    }
+
+    public void GameOverScreen()
+    {
+        SceneManager.LoadScene(3);
     }
 
     public void QuitGame()
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] float countdownTimerInSeconds = 60f;
+
+    void Update()
+    {
+        countdownTimerInSeconds -= Time.deltaTime;
+        if(countdownTimerInSeconds <= 0)
+        {
+            Debug.Log("Game Over!");
+        }
+    }
+}
